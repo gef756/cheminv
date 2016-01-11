5238c6bc4729b01e3f168567d0b73a04f7ceca17
Gabe Fernando <gef756@gmail.com>
2016-01-11 08:35:38 -0500

    ENH: create basic navbar

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+app.use('/bower_components/',
+  express.static(path.join(__dirname, 'bower_components')));
 app.use('/', routes);
 app.use('/users', users);
 
